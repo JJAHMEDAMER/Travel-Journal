23b7ac1eb02dd798678892fdefb9b6936948990d
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-04 13:54:16 +0200

    Index File setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//Style
+import "./App.css"
+
+// Components
+import App from "./App";
+
+const root = document.getElementById("root");
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  root
+);
