d038f1de4ffac790f3a615571800a3a2117b81d0
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-04 14:39:32 +0200

    App.js Setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//Comp
+import NavBar from "./comp/NavBar";
+import Card from "./comp/card";
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
